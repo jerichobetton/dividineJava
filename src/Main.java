57d6caab0f711bbccd60d5bb97c9eb1fcbde2f6f
noelcastillon1 <noelcastillon1@gmail.com>
2023-12-20 17:41:28 -0500

    add sout for input descriptions, consolidate input to single scanner

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,29 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //Create Scanner object for user input
-        Scanner usrIn = new Scanner(System.in);
-        //Create Scanner obj for tipAmt
-        Scanner usrTip = new Scanner(System.in);
-        //Declare double bill amt as user input sc.Scanner(System.in)
-        double billAmt = usrIn.nextDouble();
-        //Declare number of people as user input
-        int numOfPeople = usrIn.nextInt();
-        //Declare double tip percent as user input number / 100
-        double convertTip = usrTip.nextDouble() / 100;
-        //Create total tip amount by multiplying billAmt by convertTip
-        double tipAmt = convertTip * billAmt;
-        double totalAmt = billAmt + tipAmt;
-        //create function that adds bill amt, tipAmt / numOfPeople
-        System.out.println(totalAmt / numOfPeople);
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Dividine, your bill-splitting assistant! Press the 'Enter' key to continue.");
+        String input = scanner.nextLine();
 
+        System.out.println("Please enter the total of the bill you would like to split: ");
+        double billAmt = scanner.nextDouble();
+
+        System.out.println("Enter the percentage that you would like to tip.");
+        //convert input to percentage first
+        double convertTip = scanner.nextDouble() / 100;
+        double tipAmt = convertTip * billAmt;
+
+        //Declare number of people as user input
+        System.out.println("How many people are you splitting this bill with?");
+        int numOfPeople = scanner.nextInt();
+
+        //Create total tip amount by multiplying billAmt by convertTip
+        double totalAmt = billAmt + tipAmt;
+
+        double dividedAmt = totalAmt / numOfPeople;
+
+        //create function that adds bill amt, tipAmt / numOfPeople
+        System.out.println("Everyone owes $"+dividedAmt);
+        scanner.close();
+    }
 }
