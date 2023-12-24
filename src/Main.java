3183041582f459d80dc84ccadc9cbc639158f7d1
noelcastillon1 <noelcastillon1@gmail.com>
2023-12-23 22:45:44 -0500

    add app terminate method for user

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Dividine, your bill-splitting assistant! Press the 'Enter' key to continue.");
-        String input = scanner.nextLine();
 
-        System.out.println("Please enter the total of the bill you would like to split: ");
-        double billAmt = scanner.nextDouble();
+        while (true) {
+            System.out.println("Welcome to Dividine, your bill-splitting assistant! Press the 'Enter' key to continue.");
+            String input = scanner.nextLine();
 
-        System.out.println("Enter the percentage that you would like to tip.");
-        //convert input to percentage first
-        double convertTip = scanner.nextDouble() / 100;
-        double tipAmt = convertTip * billAmt;
+            System.out.println("Please enter the total of the bill you would like to split: ");
+            double billAmt = scanner.nextDouble();
 
-        //Declare number of people as user input
-        System.out.println("How many people are you splitting this bill with?");
-        int numOfPeople = scanner.nextInt();
+            System.out.println("Enter the percentage that you would like to tip.");
+            //convert input to percentage first
+            double convertTip = scanner.nextDouble() / 100;
+            double tipAmt = convertTip * billAmt;
 
-        //Create total tip amount by multiplying billAmt by convertTip
-        double totalAmt = billAmt + tipAmt;
+            //Declare number of people as user input
+            System.out.println("How many people are you splitting this bill with?");
+            int numOfPeople = scanner.nextInt();
 
-        double dividedAmt = totalAmt / numOfPeople;
+            //Create total tip amount by multiplying billAmt by convertTip
+            double totalAmt = billAmt + tipAmt;
 
-        //create function that adds bill amt, tipAmt / numOfPeople
-        System.out.println("Everyone owes $"+dividedAmt);
+            double dividedAmt = totalAmt / numOfPeople;
+
+            //create function that adds bill amt, tipAmt / numOfPeople
+            System.out.println("Everyone owes $" + dividedAmt);
+
+            // Check if the user wants to continue or exit after processing
+            System.out.println("Do you want to continue? (yes/no)");
+            String continueResponse =
+            continueResponse = scanner.nextLine();
+
+            if ("no".equalsIgnoreCase(continueResponse) || exitRequested(continueResponse)) {
+                System.out.println("Exiting the application...");
+                break;
+            }
+        }
         scanner.close();
+    }
+
+    private static boolean exitRequested(String userInput) {
+        if ("exit".equalsIgnoreCase(userInput)) {
+            System.out.println("Exiting the application...");
+            return true;
+        }
+        return false;
     }
 }
